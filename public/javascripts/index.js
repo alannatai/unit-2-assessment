d8dfbff4e07bc6fd8c1b1fc86eb7d33635e531d5
Alanna Tai <alanna.tai@hotmail.com>
2020-01-10 15:17:23 -0500

    fix route

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,23 +5,25 @@ $(document).ready(function() {
       $(`li#${e.target.id}`).css('text-decoration', 'line-through');
       $.ajax({
         "async": true,
-        "url": `http://localhost:3000/checkTodo/${e.target.id}`,
+        "url": `https://peaceful-stream-96627.herokuapp.com//checkTodo/${e.target.id}`,
         "method": "PUT",
         "data": { "checked": true }
       })
         .done(function (response) {
           console.log(response);
+          window.location.reload();
        });
     } else {
       $(`li#${e.target.id}`).css('text-decoration', 'none');
       $.ajax({
         "async": true,
-        "url": `http://localhost:3000/checkTodo/${e.target.id}`,
+        "url": `https://peaceful-stream-96627.herokuapp.com//checkTodo/${e.target.id}`,
         "method": "PUT",
         "data": { "checked": false }
       })
       .done(function (response) {
         console.log(response);
+        window.location.reload();
      });
     }
   });
